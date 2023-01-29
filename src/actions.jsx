7447856736b30d07c7b41b9341d2661d967a5360
themodernmonk7 <sauravavishek@gmail.com>
2023-01-29 21:08:17 +0530

    Add actions

--- src/actions.jsx
+++ src/actions.jsx
@@ -1,3 +1,4 @@
+// Sidebar actions
 export const SIDEBAR_OPEN = "SIDEBAR_OPEN"
 export const SIDEBAR_CLOSE = "SIDEBAR_CLOSE"
 
@@ -12,3 +13,21 @@ export const GET_SINGLE_PRODUCT_SUCCESS = "GET_SINGLE_PRODUCT_SUCCESS"
 export const GET_SINGLE_PRODUCT_ERROR = "GET_SINGLE_PRODUCT_ERROR"
 
 export const LOAD_PRODUCTS = "LOAD_PRODUCTS"
+
+// View actions
+export const SET_GRID_VIEW = "SET_GRID_VIEW"
+export const SET_LIST_VIEW = "SET_LIST_VIEW"
+
+// Filter actions
+export const UPDATE_SORT = "UPDATE_SORT"
+export const SORT_PRODUCTS = "SORT_PRODUCTS"
+export const UPDATE_FILTERS = "UPDATE_FILTERS"
+export const FILTER_PRODUCTS = "FILTER_PRODUCTS"
+export const CLEAR_FILTERS = "CLEAR_FILTERS"
+
+// Cart actions
+export const ADD_TO_CART = "ADD_TO_CART"
+export const REMOVE_CART_ITEM = "REMOVE_CART_ITEM"
+export const TOGGLE_CART_ITEM_AMOUNT = "TOGGLE_CART_ITEM_AMOUNT"
+export const CLEAR_CART = "CLEAR_CART"
+export const COUNT_CART_TOTALS = "COUNT_CART_TOTALS"
