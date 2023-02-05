65abeb8c3d347cffbe24489dfcf099a55ca49b23
themodernmonk7 <sauravavishek@gmail.com>
2023-02-05 16:44:31 +0530

    Update filter and clear filter

--- src/context/filter_context.jsx
+++ src/context/filter_context.jsx
@@ -19,6 +19,16 @@ const initialState = {
   all_products: [],
   grid_view: true,
   sort: "price_lowest",
+  filters: {
+    text: "",
+    company: "all",
+    category: "all",
+    color: "all",
+    min_price: 0,
+    max_price: 0,
+    price: "",
+    shipping: false,
+  },
 }
 
 const FilterContext = createContext()
@@ -31,8 +41,9 @@ export const FiltersProvider = ({ children }) => {
   }, [products])
 
   useEffect(() => {
+    dispatch({ type: FILTER_PRODUCTS })
     dispatch({ type: SORT_PRODUCTS })
-  }, [products, state.sort])
+  }, [products, state.sort, state.filters])
 
   const setGridView = () => {
     dispatch({ type: SET_GRID_VIEW })
@@ -42,13 +53,29 @@ export const FiltersProvider = ({ children }) => {
   }
 
   const updateSort = (e) => {
-    const value = e.target.value
     dispatch({ type: UPDATE_SORT, payload: value })
+  }
+
+  const updateFilters = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    dispatch({ type: UPDATE_FILTERS, payload: { name, value } })
+  }
+
+  const clearFilters = () => {
+    dispatch({ type: CLEAR_FILTERS })
   }
 
   return (
     <FilterContext.Provider
-      value={{ ...state, setGridView, setListView, updateSort }}
+      value={{
+        ...state,
+        setGridView,
+        setListView,
+        updateSort,
+        updateFilters,
+        clearFilters,
+      }}
     >
       {children}
     </FilterContext.Provider>
