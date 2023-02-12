4e634b08f20d9796cefe9d3e8ef56a626f67ac31
themodernmonk7 <sauravavishek@gmail.com>
2023-02-12 19:23:07 +0530

    Add: Remove cart item

--- src/context/cart_context.jsx
+++ src/context/cart_context.jsx
@@ -1,12 +1,37 @@
 import { createContext, useContext, useReducer } from "react"
 import reducer from "../reducers/cart_reducer"
-const initialState = {}
+import {
+  ADD_TO_CART,
+  REMOVE_CART_ITEM,
+  TOGGLE_CART_ITEM_AMOUNT,
+  CLEAR_CART,
+  COUNT_CART_TOTALS,
+} from "../actions"
+const initialState = {
+  cart: [],
+  total_items: 0,
+  total_amount: 0,
+  shipping_fee: 532,
+}
 
 const CartContext = createContext()
 export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
+
+  //   Add to Cart
+  const addToCart = (id, color, amount, product) => {
+    dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } })
+  }
+
+  // Remove item
+  const removeItem = (id) => {
+    dispatch({ type: REMOVE_CART_ITEM, payload: { id } })
+  }
+
   return (
-    <CartContext.Provider value={{ ...state }}>{children}</CartContext.Provider>
+    <CartContext.Provider value={{ ...state, addToCart, removeItem }}>
+      {children}
+    </CartContext.Provider>
   )
 }
 
--- src/reducers/cart_reducer.jsx
+++ src/reducers/cart_reducer.jsx
@@ -1,5 +1,50 @@
+import {
+  ADD_TO_CART,
+  REMOVE_CART_ITEM,
+  TOGGLE_CART_ITEM_AMOUNT,
+  CLEAR_CART,
+  COUNT_CART_TOTALS,
+} from "../actions"
 const cart_reducer = (state, action) => {
-  return state
+  if (action.type === ADD_TO_CART) {
+    const { id, color, amount, product } = action.payload
+    // Check if the item is already in the cart or not
+    let tempItem = state.cart.find((item) => item.id === id)
+    // If the item exist in the cart then I will map over the cart and check if the the cart item id matches the Id + color then I will just increase the amount else I will return the cart Item
+    if (tempItem) {
+      const tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === id + color) {
+          let newAmount = cartItem.amount + amount
+          if (newAmount > cartItem.max) {
+            newAmount = cartItem.max
+          }
+          return { ...cartItem, amount: newAmount }
+        } else {
+          return cartItem
+        }
+      })
+      return { ...state, cart: tempCart }
+      // If the item not exist in the cart then I will create a new item in the cart
+    } else {
+      const newItem = {
+        id: id + color,
+        name: product.name,
+        color,
+        amount,
+        image: product.images[0].url,
+        price: product.price,
+        max: product.stock,
+      }
+      return { ...state, cart: [...state.cart, newItem] }
+    }
+  }
+  // Remove Item
+  if (action.type === REMOVE_CART_ITEM) {
+    const { id } = action.payload
+    const tempCart = state.cart.filter((item) => item.id !== id)
+    return { ...state, cart: tempCart }
+  }
+  // return state
   throw new Error(`No matching "${action.type}" - action type `)
 }
 
