0fe8ad6d667932d7fa08b5674b6da84c7dfd7dcf
themodernmonk7 <sauravavishek@gmail.com>
2023-02-13 19:18:55 +0530

    Add Loading spinner

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,12 @@
 import React from "react"
-
+import { CgSpinner } from "react-icons/cg"
 const Loading = () => {
-  return <div>Loading...</div>
+  return (
+    <div className=" h-screen grid place-items-center ">
+      {" "}
+      <CgSpinner className=" w-10 h-10 animate-spin text-green-600" />{" "}
+    </div>
+  )
 }
 
 export default Loading
