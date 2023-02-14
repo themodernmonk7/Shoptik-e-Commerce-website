2ec028b3971cb0c6ffff11c5819cd456c948ad79
themodernmonk7 <sauravavishek@gmail.com>
2023-02-14 17:13:30 +0530

    Add: Auth0 package setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { Auth0Provider } from "@auth0/auth0-react"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
 import { CartProvider } from "./context/cart_context"
 import { FiltersProvider } from "./context/filter_context"
 import { ProductsProvider } from "./context/products_context"
-import './index.css'
-
+import "./index.css"
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ProductsProvider>
-      <FiltersProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </FiltersProvider>
-    </ProductsProvider>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+      // cacheLocation="localStorage"
+    >
+      <ProductsProvider>
+        <FiltersProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FiltersProvider>
+      </ProductsProvider>
+    </Auth0Provider>
   </React.StrictMode>
 )
