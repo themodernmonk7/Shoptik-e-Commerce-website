abfae89ea5e4d3603c9f80a7554e5e9fae8d56c2
themodernmonk7 <sauravavishek@gmail.com>
2023-02-14 11:12:55 +0530

    Add image component

--- src/components/List_view_products.jsx
+++ src/components/List_view_products.jsx
@@ -1,28 +1,18 @@
 import React from "react"
-import { Link } from "react-router-dom"
-import { formatPrice } from "../utils/helper"
+import { ProductImage, Product_title } from "../components"
 
 const List_view_products = ({ products }) => {
   return (
     <>
       <section className=" grid grid-cols-1 gap-6">
         {products.map((product) => {
-          const { id, name, image, price, description } = product
+          const { id, description } = product
           return (
-            <article key={id} className="flex flex-row gap-10  ">
-              <div className=" bg-gray-100 w-1/2  ">
-                <Link to={`/shop/:${id}`}>
-                  <img
-                    src={image}
-                    alt={name}
-                    className=" w-96 h-80 object-cover object-center mix-blend-darken p-8 "
-                  />
-                </Link>
-              </div>
-              <div className=" space-y-4 w-full  ">
-                <p className=" font-medium  capitalize"> {name}</p>
-                <p className=" font-light  "> {formatPrice(price)} </p>
-                <p className="  "> {description} </p>
+            <article key={id} className=" flex flex-row gap-10 group ">
+              <ProductImage product={product} className="w-96 h-80 p-10 " />
+              <div className=" w-full ">
+                <Product_title product={product} className="w-full" />
+                <p className="  mt-10 "> {description} </p>
               </div>
             </article>
           )
