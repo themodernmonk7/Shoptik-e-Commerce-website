047f242455f06f6635877b5f085ce51260939559
themodernmonk7 <sauravavishek@gmail.com>
2023-02-24 09:49:44 +0530

    Fix color

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ import { useProductsContext } from "../context/products_context"
 const Sidebar = () => {
   const { closeSidebar } = useProductsContext()
   return (
-    <aside className=" fixed  h-screen z-30 top-0 right-0 left-0   w-2/3 bg-gray-100 md:hidden overflow-hidden px-5 space-y-5 shadow-xl ">
+    <aside className=" fixed  h-screen z-30 top-0 right-0 left-0  w-2/3 bg-white md:hidden overflow-hidden px-5 space-y-5 shadow-xl ">
       <div className=" py-4 flex justify-between items-center ">
         <Logo />
         <button
