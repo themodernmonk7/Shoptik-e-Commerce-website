35aea32c8af6d7d021a09051289396740776c3bc
themodernmonk7 <sauravavishek@gmail.com>
2023-02-25 19:27:56 +0530

    Fix amount bug

--- functions/stripe-payment-intent.js
+++ functions/stripe-payment-intent.js
@@ -10,10 +10,10 @@ exports.handler = async (event, context) => {
     // Replace this constant with a calculation of the order's amount
     // Calculate the order total on the server to prevent
     // people from directly manipulating the amount on the client
-    return shipping_fee + total_amount
+    return (shipping_fee + total_amount) * 100
   }
   try {
-    // Create PaymentIntent with the order amount and currency
+    // Create a PaymentIntent with the order amount and currency
     const paymentIntent = await stripe.paymentIntents.create({
       amount: calculateOrderAmount(),
       currency: "INR",
