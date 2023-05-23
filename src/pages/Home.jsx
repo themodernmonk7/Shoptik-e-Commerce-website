e76558ca0225b626d90ad549a189cc9538064662
themodernmonk7 <sauravavishek@gmail.com>
2023-05-23 16:18:31 +0530

    optimize code using lazy loading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,28 @@
-import React from "react"
-import {
-  BasketProduct,
-  Header,
-  HomeProduct,
-  Instagram,
-  Loading,
-  ProductCategory,
-  ProductGrid,
-} from "../components"
+import React, { Suspense, lazy } from "react"
+
 import { useProductsContext } from "../context/products_context"
+import { Loading } from "../components"
+
+const HomeProduct = lazy(() => import("../components/HomeProduct"))
+const BasketProduct = lazy(() => import("../components/Basket"))
+const Header = lazy(() => import("../components/Header"))
+const Instagram = lazy(() => import("../components/Instagram"))
+const ProductCategory = lazy(() => import("../components/ProductCategory"))
+const ProductGrid = lazy(() => import("../components/ProductGrid"))
 
 const Home = () => {
   const { products_loading: loading } = useProductsContext()
   if (loading) return <Loading />
   return (
     <>
-      <Header />
-      <HomeProduct />
-      <BasketProduct />
-      <ProductGrid />
-      <ProductCategory />
-      <Instagram />
+      <Suspense fallback={<Loading />}>
+        <Header />
+        <HomeProduct />
+        <BasketProduct />
+        <ProductGrid />
+        <ProductCategory />
+        <Instagram />
+      </Suspense>
     </>
   )
 }
